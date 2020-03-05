66ece85f114b4b68b5bc52e7e704ca373e55f9e9
Christian Lauli <christian.lauli@googlemail.com>
2020-03-05 01:25:33 +0100

    added starting sequence

--- Final_TempSensor.ino
+++ Final_TempSensor.ino
@@ -42,6 +42,8 @@ void setup()
 
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
+
+  rainbow(10);
 }
 
 void getTemperature() {
@@ -63,32 +65,50 @@ void setColor(int led, int redValue, int greenValue, int blueValue, int delayVal
 }
 
 
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<pixels.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / pixels.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV(pixelHue)));
+    }
+    pixels.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
 void loop() {
  delay(3000);
  getTemperature();
  Serial.printf(temperatureCString);
  Serial.println(" Grad Celsisus");
 
-  
   int led;
   for(led=0; led <=12; led++)
   {
-    if(temperatureC>28.00){
+    if(temperatureC>=28.00){
     setColor(led,200,0,0,300); //red
     }
-
+    if(temperatureC>=25.00&&temperatureC<28.00){
+    setColor(led,180,180,0,300); //yellow
+    }
+    if(temperatureC>=20.00&&temperatureC<25.00){
+    setColor(led,80,80,80,300); //white
+    }
     if(temperatureC<20.00){
     setColor(led,0,0,200,300); //blue
     }
-
-    if(temperatureC>20.00&&temperatureC<25.00){
-    setColor(led,80,80,80,300); //white
-    }
-
-    if(temperatureC>25.00&&temperatureC<28.00){
-    setColor(led,180,180,0,300); //yellow
-    }
-
-}
- 
+  }
 }
